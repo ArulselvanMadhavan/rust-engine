493a9eded42491fd385a69e5be13502cec4c1e02
jlyneu <jly@ccs.neu.edu>
2016-03-26 00:05:16 -0400

    Server returns file based on requested path if file exists, else error

--- playground/web_server/src/main.rs
+++ playground/web_server/src/main.rs
@@ -3,8 +3,34 @@ use std::thread;
 use std::io::prelude::*;
 use std::fs::File;
 use std::str;
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
+
+    // Global error file variables. Write error_file_str to
+    // stream when requested file is not found
+    
+
+    fn get_path_from_request(request: &str) -> &str {
+
+        let mut abs_path = PathBuf::new();
+        let curr_dir = env::current_dir().unwrap();
+        abs_path.push(curr_dir);
+        println!("{}", abs_path.display());
+        let mut iter = request.split_whitespace();
+        iter.next();
+        let requested_path = iter.next().unwrap();
+        abs_path.push(requested_path);
+        println!("{}", abs_path.display());
+        abs_path;
+        // TODO: Rewrite to avoid creating new string
+        &requested_path[1..]
+
+        //for word in request.split_whitespace() {
+        //    println!("{:?}", word);
+        //}
+    }
 
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
@@ -13,29 +39,39 @@ fn main() {
         let mut request = String::new();
         println!("Ready to read");
        
-        let mut contents = [0; 1024];
+        // TODO: Content length should be inferred from the request, not hardcoded
+        let mut contents = [0; 16];
+
         // read request into string
         let result = stream.read(&mut contents);
         match result {
             Ok(result) => {
-                // convert byte array to byte vector
-                // convert byte vector to string
                 let string_result = str::from_utf8(&contents).unwrap();
-                println!("{:?}", string_result);
-                println!("{:?}", result);
+                let pathname = get_path_from_request(string_result);
+                println!("{:?}", pathname);
+                let f = match File::open(pathname) {
+                    Ok(mut f) => {
+                        println!("OK!");
+                        let mut s = String::new();
+                        f.read_to_string(&mut s);
+                        println!("{}",s);
+                        stream.write(s.as_bytes());
+                    }
+                    Err(e) => {
+                        println!("NOT OK!");
+                        let mut error_file = File::open("error.html").unwrap();
+                        let mut error_vec = Vec::new();
+                        error_file.read_to_end(&mut error_vec);
+                        let error_byte_array = error_vec.as_slice();
+                        stream.write(error_byte_array);
+                    }
+                };
             }
             Err(e) => { 
-                println!("Error when reading from stream");
+                println!("Error when reading request");
             }
         }
-        //stream.read_to_string(&mut request).unwrap();
-        //println!("{:?}", request);
-        let mut f = File::open("foo.txt").unwrap();
-        let mut s = String::new();
-        f.read_to_string(&mut s);
-        println!("{}",s);
-        stream.write(s.as_bytes());
-        println!("handling client!");
+        
     }
 
     // accept connections and process them, spawning a new thread for each one
