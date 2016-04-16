c813f7270d93a426c1149fb70412fbc3f5349ef8
Arulselvan <arulselv@ccs.neu.edu>
2016-04-16 13:56:45 -0400

    experimenting

--- playground/big-int/src/main.rs
+++ playground/big-int/src/main.rs
@@ -5,6 +5,10 @@ pub struct BigInt {
     pub data: Vec<u64>,
 }
 
+pub trait Minimum{
+    fn min<'a>(&'a self, other: &'a Self) -> &'a Self;
+}
+
 impl BigInt {
     pub fn new(x: u64) -> Self {
         if x == 0 {
@@ -47,7 +51,7 @@ impl Clone for BigInt {
 }
 
 impl BigInt {
-    pub fn min_try1(self, other: Self) -> Self {
+    pub fn min_try1<'a>(&'a self, other: &'a Self) -> &'a Self {
         debug_assert!(self.test_invariant() && other.test_invariant());
         if self.data.len() < other.data.len() {
             self
@@ -58,32 +62,48 @@ impl BigInt {
         }
     }
 
-    pub fn vec_min(v: &Vec<BigInt>) -> Option<BigInt> {
-        let mut min: Option<BigInt> = None;
-        for e in v.iter() {
-            let e = e.clone();
+
+    pub fn vec_min<T: Minimum>(v: &Vec<T>) -> Option<&T> {
+        let mut min: Option<&T> = None;
+        for e in v {
             min = match min {
                 None => Some(e),
-                Some(n) => Some(e.min_try1(n)),
+                Some(n) => Some(n.min(e)),
             };
         }
         min
     }
 
-    pub fn head<T>(v:&Vec<T>)->Option<&T>{
-        if v.len() > 0{
+    pub fn head<T>(v: &Vec<T>) -> Option<&T> {
+        if v.len() > 0 {
             Some(&v[0])
-        }else{
+        } else {
             None
         }
     }
 }
+
+impl Minimum for BigInt {
+    fn min<'a>(&'a self, other: &'a Self) -> &'a Self {
+        debug_assert!(self.test_invariant() && other.test_invariant());
+        if self.data.len() < other.data.len() {
+            self
+        } else if self.data.len() > other.data.len() {
+            other
+        } else {
+            self
+        }
+    }
+}
 pub fn main() {
-    let v = vec![1, 0, 1 << 16, 0, 0, 0, 0];
-    let b1 = BigInt::from_vec((&v).clone());
+    let v1 = vec![1, 0, 1 << 16, 0, 0, 0, 0];
+    let v2 = vec![1, 0, 234];
+    let b1 = BigInt::from_vec((&v1).clone());
     println!("{:?}", b1);
     println!("{:?}", b1.test_invariant());
-    let b2 = BigInt::from_vec((&v).clone());
+    let b2 = BigInt::from_vec((&v2).clone());
     println!("{:?}", b2.test_invariant());
-    println!("{:?}", BigInt::head(&v).unwrap());
+    println!("{:?}", BigInt::head(&v1).unwrap());
+    let bv: Vec<BigInt> = vec![b1, b2];
+    println!("{:?}", BigInt::vec_min(&bv).unwrap());
 }
