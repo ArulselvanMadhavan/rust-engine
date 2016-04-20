cc81432e8eed6994734db5a09bcb2179187a1d52
Arulselvan <arulselv@ccs.neu.edu>
2016-04-20 19:26:04 -0400

    Basic test code for cache implementation 1

--- playground/cache/src/main.rs
+++ playground/cache/src/main.rs
@@ -1,5 +1,6 @@
 extern crate concurrent_hashmap;
 extern crate rand;
+extern crate chrono;
 
 use std::thread;
 use std::default::Default;
@@ -11,32 +12,42 @@ use std::sync::mpsc::{Sender, Receiver, channel};
 use std::error::Error;
 use std::thread::sleep;
 use std::time::Duration;
+use chrono::UTC;
+use chrono::datetime::DateTime;
+
+const CACHE_THRESHOLD: u8 = 50;
+
+#[derive(Debug)]
 struct Cache {
-    key: String,
-    value: u32,
+    data: String,
+    last_modified: DateTime<UTC>,
 }
 
-fn get_cache_objects(num_objects: usize) -> Vec<String> {
+fn get_cache_objects(num_objects: usize) -> Vec<(String)> {
     let mut cache_objects: Vec<String> = Vec::with_capacity(num_objects);
     let mut rng = rand::thread_rng();
     for _ in 0..num_objects {
-        cache_objects.push(format!("Cache_object_{}", rng.gen::<u8>()));
+        let file_size = rng.gen::<u8>();
+        let key = format!("File_PATH_{}", file_size);
+        cache_objects.push(key);
     }
     cache_objects
 }
 fn main() {
     let num_objects: usize = 10;
-    let num_iterations = 10000;
+    let num_iterations = 20;
     let nthreads: usize = 4;
     let mut threads = Vec::with_capacity(nthreads);
 
     let cache_objects = Arc::new(get_cache_objects(num_objects));
-    let cache: Arc<ConcHashMap<String, u32>> = Default::default();
-    let (tx, rx) = channel::<Cache>();
+    let cache: Arc<ConcHashMap<String, Cache>> = Default::default();
+    let (tx, rx) = channel::<usize>();
     let rx = Arc::new(Mutex::new(rx));
+    // TODO-Spawn threads in separate method
     for _ in 0..nthreads {
         let rx = rx.clone();
         let cache_copy = cache.clone();
+        let cache_objects_copy = cache_objects.clone();
         threads.push(thread::spawn(move || {
             loop {
                 let message = {
@@ -44,16 +55,28 @@ fn main() {
                     job.recv()
                 };
                 match message {
-                    Ok(job) => {
-                        let val = cache_copy.find(&job.key);
+                    Ok(id) => {
+                        let ref obj = cache_objects_copy[id];
+                        let key = obj.to_owned();
+
+                        // let (key, cache_obj) = obj.to_owned();
+                        let val = cache_copy.find(&key);
                         match val {
-                            Some(_) => {
-                                // println!("Cache hit for {:?}", acc.get());
-                                // cache_copy.insert(job.key.to_owned(), acc.get()+1);
-                            },
+                            Some(acc) => {
+                                println!("Reading the {:?}\t{:?}",
+                                         acc.get().data,
+                                         acc.get().last_modified);
+                            }
                             None => {
                                 println!("Element not found");
-                                cache_copy.upsert(job.key.to_owned(), 1, &|count| *count += 1);
+                                // Open a file
+                                // Create a cache object
+                                // Save it in the cache(hash map)
+                                let cache_obj = Cache {
+                                    data: format!("Data in {}", key),
+                                    last_modified: UTC::now(),
+                                };
+                                cache_copy.insert(key, cache_obj);
                             }
                         }
                     }
@@ -68,12 +91,8 @@ fn main() {
     let between = Range::new(0, num_objects);
     let mut rng = rand::thread_rng();
     for _ in 0..num_iterations {
-        let element_id = between.ind_sample(&mut rng);
-        let cache_obj = Cache {
-            key: cache_objects[element_id].to_owned(),
-            value: 0,
-        };
-        tx.send(cache_obj).unwrap();
+        let element_id: usize = between.ind_sample(&mut rng);
+        tx.send(element_id).unwrap();
     }
 
     // for _ in 0..nthreads{
@@ -104,7 +123,7 @@ fn main() {
     //     thread.join().unwrap();
     // }
 
-    sleep(Duration::new(5, 0));
+    sleep(Duration::new(10, 0));
     println!("Sleep ends");
     for cache_item in cache.iter() {
         println!("{:?}", cache_item);
